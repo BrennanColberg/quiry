db14872eaab1eee8c5098795b0a191a6f9e60fad
Brennan Colberg <brennan.colberg@gmail.com>
2020-07-29 05:20:46 -0700

    deleted AllHistory

--- components/RecentHistory.tsx
+++ components/RecentHistory.tsx
@@ -1,10 +1,10 @@
 import styles from '../styles/QuestionHistory.module.css'
 import { useEffect, useState } from 'react'
 import { authClient, dbClient } from '../firebase'
-import Question from './Question'
 import useRecentAmount from '../hooks/useRecentAmount'
+import Question from './Question'
 
-export const RecentHistory = (): JSX.Element => {
+export default (): JSX.Element => {
   const [userId, setUserId] = useState(undefined)
   const [questions, setQuestions] = useState([])
   const [, setQuestionsListener] = useState(undefined)
@@ -44,49 +44,6 @@ export const RecentHistory = (): JSX.Element => {
     <ul className={styles.list}>
       {questions.map((question, i) => (
         <Question question={question} key={i} i={i} />
-      ))}
-    </ul>
-  )
-}
-
-export const AllHistory = (): JSX.Element => {
-  const [userId, setUserId] = useState(undefined)
-  const [questions, setQuestions] = useState([])
-  const [, setQuestionsListener] = useState(undefined)
-
-  // keep track of authenticated user
-  useEffect(() => {
-    authClient.onAuthStateChanged((user) => {
-      if (user !== null) {
-        setUserId(user.uid)
-      } else {
-        setUserId(undefined)
-      }
-    })
-  }, [])
-
-  // keep track of said user's questions
-  useEffect(() => {
-    setQuestionsListener((unsub) => {
-      if (unsub) unsub()
-      setQuestions([])
-      if (userId)
-        return dbClient
-          .collection('questions')
-          .where('author', '==', userId)
-          .orderBy('created', 'desc')
-          .onSnapshot((snap) =>
-            setQuestions(
-              snap.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-            )
-          )
-    })
-  }, [userId])
-
-  return (
-    <ul className={styles.list}>
-      {questions.map((question, i) => (
-        <Question question={question} key={i} />
       ))}
     </ul>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import styles from '../styles/index.module.css'
 import QuestionForm from '../components/QuestionForm'
-import { RecentHistory } from '../components/QuestionHistory'
+import RecentHistory from '../components/RecentHistory'
 import TodayCounter from '../components/GoalProgress'
 import LoginButton from '../components/buttons/LoginButton'
 import FeedbackButton from '../components/buttons/FeedbackButton'
-import Link from 'next/link'
 
 export default (): JSX.Element => (
   <>
@@ -13,9 +12,6 @@ export default (): JSX.Element => (
     <RecentHistory />
 
     <div className={styles.buttons}>
-      <Link href="/history" passHref>
-        <a>see all history</a>
-      </Link>
       <LoginButton />
       <FeedbackButton />
     </div>
