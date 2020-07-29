b7f92d48acce6dc011662ea071e10362514aa415
Brennan Colberg <brennan.colberg@gmail.com>
2020-07-29 07:27:35 -0700

    fixed positioning of delete button

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,8 +1,5 @@
 import styles from '../styles/Question.module.scss'
-import { useState } from 'react'
-import classNames from 'classnames'
 import { dbClient, analyticsClient } from '../firebase'
-import { isTimestampToday } from '../utils'
 import { firestore } from 'firebase/app'
 
 export default ({
@@ -10,8 +7,6 @@ export default ({
 }: {
   question: { text: string; id: string; created: firestore.Timestamp }
 }): JSX.Element => {
-  const [hovered, setHovered] = useState(false)
-
   async function triggerDelete() {
     if (confirm(`Are you sure you want to delete "${question.text}"?`)) {
       await dbClient.collection('questions').doc(question.id).delete()
@@ -29,22 +24,11 @@ export default ({
     await ref.update({ text: text.trim() })
   }
 
-  const DeleteButton = () => (
-    <span onClick={triggerDelete} className={styles.delete}>
-      ❌
-    </span>
-  )
-
   return (
-    <li
-      className={classNames(styles.question, {
-        [styles.hovered]: hovered,
-        [styles.today]: isTimestampToday(question.created),
-      })}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <DeleteButton />
+    <li className={styles.question}>
+      <span onClick={triggerDelete} className={styles.delete}>
+        ❌
+      </span>
       <span
         contentEditable
         suppressContentEditableWarning
